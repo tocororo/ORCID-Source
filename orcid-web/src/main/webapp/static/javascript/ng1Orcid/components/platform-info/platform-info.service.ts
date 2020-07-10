809e6167c7bff02c86d09ac1a853ef8343bfe7c2
Rafael Martinez Estevez <rafael.martinez@upr.edu.cu>
2020-07-10 15:51:52 -0400

    build angular_orcid_generated.js without bit.dev node.bit.dev is bocked in cuba

---
+++ orcid-web/src/main/webapp/static/javascript/ng1Orcid/components/platform-info/platform-info.service.ts
@@ -0,0 +1,105 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout'
+import { Platform } from '@angular/cdk/platform'
+import { Injectable, LOCALE_ID, Inject } from '@angular/core'
+import { BehaviorSubject, Observable } from 'rxjs'
+import { PlatformInfo } from './platform-info.type'
+import { BROWSERLIST_REGEXP } from './browserlist.regexp'
+
+@Injectable()
+export class PlatformInfoService {
+  platformSubject = new BehaviorSubject<PlatformInfo>(null)
+
+  platform: PlatformInfo = {
+    unsupportedBrowser: false,
+    desktop: false,
+    tabletOrHandset: false,
+    tablet: false,
+    handset: false,
+    edge: false,
+    ie: false,
+    firefox: false,
+    safary: false,
+    columns4: false,
+    columns8: false,
+    columns12: false,
+    rtl: false,
+    ltr: true,
+    screenDirection: 'ltr',
+  }
+
+  constructor(
+    @Inject(LOCALE_ID) public locale: string,
+    private _breakpointObserver: BreakpointObserver,
+    _platform: Platform
+  ) {
+    this.platform.rtl = locale === 'ar' ? true : false
+    this.platform.ltr = !this.platform.rtl
+    this.platform.screenDirection = this.platform.rtl ? 'rtl' : 'ltr'
+
+    if (!BROWSERLIST_REGEXP.test(navigator.userAgent)) {
+      this.platform.unsupportedBrowser = true
+    }
+
+    this.platform.firefox = _platform.FIREFOX
+    this.platform.safary = _platform.SAFARI
+    this.platform.ie = _platform.TRIDENT
+    this.platform.edge = _platform.EDGE
+    this.platformSubject.next(this.platform)
+
+    this._breakpointObserver
+      .observe([Breakpoints.Handset])
+      .subscribe((state) => {
+        if (state.matches) {
+          this.platform.handset = true
+        } else {
+          this.platform.handset = false
+        }
+        this.platformSubject.next(this.platform)
+      })
+    this._breakpointObserver
+      .observe([Breakpoints.Tablet])
+      .subscribe((state) => {
+        if (state.matches) {
+          this.platform.tablet = true
+        } else {
+          this.platform.tablet = false
+        }
+        this.platformSubject.next(this.platform)
+      })
+    this._breakpointObserver
+      .observe([Breakpoints.Handset, Breakpoints.Tablet])
+      .subscribe((state) => {
+        if (state.matches) {
+          this.platform.tabletOrHandset = true
+          this.platform.desktop = false
+        } else {
+          this.platform.tabletOrHandset = false
+          this.platform.desktop = true
+        }
+        this.platformSubject.next(this.platform)
+      })
+
+    this._breakpointObserver
+      .observe(['(min-width: 839.99px)', '(min-width: 599.99px)'])
+      .subscribe((state) => {
+        if (state.breakpoints['(min-width: 839.99px)']) {
+          this.platform.columns8 = false
+          this.platform.columns12 = true
+          this.platform.columns4 = false
+        } else if (state.breakpoints['(min-width: 599.99px)']) {
+          this.platform.columns8 = true
+          this.platform.columns12 = false
+          this.platform.columns4 = false
+        } else {
+          this.platform.columns8 = false
+          this.platform.columns12 = false
+          this.platform.columns4 = true
+        }
+        this.platformSubject.next(this.platform)
+      })
+  }
+
+  public get(): Observable<PlatformInfo> {
+    return this.platformSubject.asObservable()
+  }
+}
---
+++ orcid-web/src/main/webapp/static/javascript/ng1Orcid/components/platform-info/platform-info.type.ts
@@ -0,0 +1,17 @@
+export interface PlatformInfo {
+  rtl: boolean
+  ltr: boolean
+  screenDirection: 'rtl' | 'ltr'
+  unsupportedBrowser: boolean
+  desktop: boolean
+  tabletOrHandset: boolean
+  tablet: boolean
+  handset: boolean
+  edge: boolean
+  ie: boolean
+  safary: boolean
+  firefox: boolean
+  columns4: boolean
+  columns8: boolean
+  columns12: boolean
+}
